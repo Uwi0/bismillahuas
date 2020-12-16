42751d12e36e4d96d1583aef4df10a6c99fd5e45
zicchi <zidanezicchi@gmail.com>
2020-12-16 19:04:17 +0700

    cok kenek

--- app/src/main/java/com/kakapo/squizapp/model/Category.kt
+++ app/src/main/java/com/kakapo/squizapp/model/Category.kt
@@ -2,6 +2,6 @@ package com.kakapo.squizapp.model
 
 class Category(
     var Name: String? = null,
-    var Image: String? = null,
+//    var Image: String? = null,
 ) {
 }
--- app/src/main/java/com/kakapo/squizapp/ui/CategoryFragment.kt
+++ app/src/main/java/com/kakapo/squizapp/ui/CategoryFragment.kt
@@ -1,18 +1,22 @@
 package com.kakapo.squizapp.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import com.kakapo.squizapp.R
 import com.kakapo.squizapp.adapter.CategoryAdapter
+import com.kakapo.squizapp.common.Common
 import com.kakapo.squizapp.model.Category
+import com.kakapo.squizapp.model.User
 
 
 class CategoryFragment : Fragment() {
@@ -30,21 +34,31 @@ class CategoryFragment : Fragment() {
         myFragment = inflater.inflate(R.layout.fragment_category, container, false)
 
         listCategory = myFragment.findViewById(R.id.listCategory)
-        listCategory.setHasFixedSize(true)
+//        listCategory.setHasFixedSize(true)
         layoutManager = LinearLayoutManager(container!!.context)
         listCategory.layoutManager = layoutManager
-
-        loadCategories()
+        mBase = FirebaseDatabase.getInstance().reference.child("Category")
+        loadCategory()
 
         return myFragment
     }
 
     private fun loadCategories(){
-        mBase = FirebaseDatabase.getInstance().reference
+
         val options:FirebaseRecyclerOptions<Category> = FirebaseRecyclerOptions
                 .Builder<Category>()
                 .setQuery(mBase, Category::class.java)
                 .build()
+        adapter = CategoryAdapter(options)
+        listCategory.adapter = adapter
+    }
+
+    private fun loadCategory(){
+        val options = FirebaseRecyclerOptions.Builder<Category>()
+            .setQuery(mBase,Category::class.java)
+            .setLifecycleOwner(this)
+            .build()
+
         adapter = CategoryAdapter(options)
         listCategory.adapter = adapter
     }
